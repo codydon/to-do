8ed389ba9d96a142c19078349a893380bd1735fb
codydon <donaldonserio20@gmail.com>
2023-02-05 16:13:36 +0300

    dark mode button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -51,7 +51,7 @@ export default function Home() {
                     <div className='flex  gap-4'>
 
                         <div>
-                            <button className="bg-black hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={toggletheme}
+                            <button className="bg-black hover:bg-gray-300 hover:text-black text-white font-bold py-2 px-4 rounded" onClick={toggletheme}
                             >light/dark</button>
                         </div>
                         <div>
